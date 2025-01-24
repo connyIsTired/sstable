9cd44d5addc4ebf30a4fe754705067d734f21ca9
Conny <conradkersten@gmail.com>
2025-01-24 08:28:49 -0600

    Fix names and propogate errors

--- internal/trees/redblack.go
+++ internal/trees/redblack.go
@@ -1,13 +1,21 @@
 package trees
 
-import "errors"
+import (
+	"errors"
+)
 
 const (
 	Red color = iota
 	Black
 )
 
+const (
+	LeftChild childType = iota
+	RightChild
+)
+
 type color int
+type childType int
 
 type rbTree struct {
 	Root *node
@@ -20,10 +28,11 @@ type node struct {
 	LeftChild  *node
 	RightChild *node
 	Color      color
+	ChildType  childType
 }
 
 func NewRBTree(key int, value int) *rbTree {
-	node := &node{Color: Red, Key: key, Value: value}
+	node := &node{Color: Black, Key: key, Value: value}
 	return &rbTree{Root: node}
 }
 
@@ -31,30 +40,38 @@ func (t *rbTree) Insert(key int, value int) error {
 	return t.Root.insert(key, value)
 }
 
-func (n *node) insert(key int, value int) error {
-	if key > n.Key {
-		if n.RightChild == nil {
-			newnode := &node{Key: key, Value: value}
-			n.RightChild = newnode
-			newnode.Parent = n
-			return nil
+func (parentNode *node) insert(key int, value int) error {
+	if key > parentNode.Key {
+		if parentNode.RightChild == nil {
+			newnode := &node{Key: key, Value: value, Color: Red}
+			parentNode.RightChild = newnode
+			newnode.Parent = parentNode
+			newnode.ChildType = RightChild
+			return newnode.balance()
 		}
-		n.RightChild.insert(key, value)
-		return nil
+		return parentNode.RightChild.insert(key, value)
 	}
-	if key < n.Key {
-		if n.LeftChild == nil {
-			newnode := &node{Key: key, Value: value}
-			n.LeftChild = newnode
-			newnode.Parent = n
-			return nil
+	if key < parentNode.Key {
+		if parentNode.LeftChild == nil {
+			newnode := &node{Key: key, Value: value, Color: Red}
+			parentNode.LeftChild = newnode
+			newnode.Parent = parentNode
+			newnode.ChildType = LeftChild
+			return newnode.balance()
 		}
-		n.LeftChild.insert(key, value)
-		return nil
+		return parentNode.LeftChild.insert(key, value)
 	}
-	if key == n.Key {
-		n.Value = value
+	if key == parentNode.Key {
+		parentNode.Value = value
 		return nil
 	}
 	return errors.New("Node could not be inserted")
+}
+
+func (n *node) balance() error {
+	if n.Parent.Color == Black {
+		return nil
+	}
+
+	return errors.New("Could not balance")
 }
--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	index := trees.NewRBTree(10, 0)
-	index.Insert(15, 0)
-	index.Insert(20, 0)
-	index.Insert(5, 0)
-	fmt.Printf("%v %v %v %v", index.Root.LeftChild.Key, index.Root.Key, index.Root.RightChild.Key, index.Root.RightChild.RightChild.Key)
+	err := index.Insert(5, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = index.Insert(1, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
